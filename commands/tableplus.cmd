e7e27c14fd1c7106703eed9ea30de594e6f0946a
Rick Bouma <r.bouma@epartment.nl>
2022-08-10 20:40:52 +0200

    refactor images and commands

--- commands/tableplus.cmd
+++ commands/tableplus.cmd
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 [[ ! ${ROLL_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
-# Update commands check.
-"${ROLL_DIR}/bin/roll" cli-tools-update-check
-clear
 
 ROLL_ENV_PATH="$(locateEnvPath)" || exit $?
 loadEnvConfig "${ROLL_ENV_PATH}" || exit $?
