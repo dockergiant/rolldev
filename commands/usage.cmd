f9e0ba03e3193d56a06f207e78b815cfbad16159
Rick Bouma <r.bouma@epartment.nl>
2022-09-11 15:59:42 +0200

    change xdebug configuration files

--- commands/usage.cmd
+++ commands/usage.cmd
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 [[ ! ${ROLL_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
 
+## local project directory if running within one; don't fail if it can't be found
+ROLL_ENV_PATH="$(locateEnvPath 2>/dev/null)" || true
+
+MAGENTO_HELP=""
+if [[ -n "$ROLL_ENV_PATH" ]];then
+	loadEnvConfig "${ROLL_ENV_PATH}" || true
+	if [[ -n "$ROLL_ENV_TYPE" && "${ROLL_ENV_TYPE}" == "magento2" ]]; then
+    source "${ROLL_DIR}/commands/magento2-usage.help"
+  fi
+fi
+
+
 ## load usage info for the given command falling back on default usage text
 if [[ -f "${ROLL_CMD_HELP}" ]]; then
   source "${ROLL_CMD_HELP}"
