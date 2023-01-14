f28c9a385ca88af3b7935b0601455b49acdff0d7
Rick Bouma <r.bouma@epartment.nl>
2023-01-14 17:11:20 +0100

    Reworked github actions + build pipeline

---
+++ .github/workflows/php-matrix/constants.php
@@ -0,0 +1,30 @@
+<?php
+
+const PHP_LATEST = '8.2';
+const PHP_VERSIONS = ['7.0', '7.1', '7.2', '7.3', '7.4', '8.0', '8.1', '8.2'];
+CONST PHP_VERSIONS_OS_RELEASE = [
+    '7.0' => 'stretch',
+    '7.1' => 'buster',
+    '7.2' => 'buster',
+    '7.3' => 'bullseye',
+    '7.4' => 'bullseye',
+    '8.0' => 'bullseye',
+    '8.1' => 'bullseye',
+    '8.2' => 'bullseye'
+];
+const NODE_LATEST = '19';
+const NODE_VERSIONS = ['10', '12', '13', '14', '15', '16', '17', '18', '19'];
+CONST NODE_VERSIONS_OS_RELEASE = [
+    '10' => 'stretch',
+    '12' => 'bullseye',
+    '13' => 'buster',
+    '14' => 'bullseye',
+    '15' => 'buster',
+    '16' => 'bullseye',
+    '17' => 'bullseye',
+    '18' => 'bullseye',
+    '19' => 'bullseye'
+];
+const EXPERIMENTAL_PHP_VERSIONS = [];
+const NOT_STABLE_XDEBUG_PHP_VERSIONS = ['7.0', '7.1', '7.2', '7.3', '7.4'];
+
